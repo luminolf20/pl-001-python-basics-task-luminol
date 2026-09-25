2ae02ddf04f643936610140d29396116dba4971a
Luminol <luminol.f.20@gmail.com>
2026-09-25 23:40:07 +0300

    Solve task 1

--- src/part1/snowflake.py
+++ src/part1/snowflake.py
@@ -2,8 +2,10 @@ import time
 
 from .constants import (
     EPOCH_MS_DEFAULT,
+    NODE_ID_BITS,
     NODE_ID_DEFAULT,
     NODE_ID_MAX,
+    SEQUENCE_ID_BITS,
     SEQUENCE_ID_MAX,
     TIMESTAMP_MS_MAX,
 )
@@ -14,12 +16,12 @@ def read_current_millis(epoch_ms: int = EPOCH_MS_DEFAULT) -> int:
 
 
 def decode_timestamp_ms(snowflake_id: int, epoch_ms: int = EPOCH_MS_DEFAULT) -> int:
-    current_time = (snowflake_id >> (NODE_ID_MAX + SEQUENCE_ID_MAX)) + epoch_ms
+    current_time = (snowflake_id >> (NODE_ID_BITS + SEQUENCE_ID_BITS)) + epoch_ms
     return current_time
 
 
 def decode_node_id(snowflake_id: int) -> int:
-    node_id = (snowflake_id >> SEQUENCE_ID_MAX) & NODE_ID_MAX
+    node_id = (snowflake_id >> SEQUENCE_ID_BITS) & NODE_ID_MAX
     return node_id
 
 
@@ -44,8 +46,8 @@ def generate_snowflake_id(
     else:
         if epoch_ms <= read_current_millis(epoch_ms) <= TIMESTAMP_MS_MAX:
             snowflake_id = (
-                (read_current_millis(epoch_ms) << (NODE_ID_MAX + SEQUENCE_ID_MAX))
-                | (node_id << (SEQUENCE_ID_MAX))
+                (read_current_millis(epoch_ms) << (NODE_ID_BITS + SEQUENCE_ID_BITS))
+                | (node_id << (SEQUENCE_ID_BITS))
                 | sequence_id
             )
             return snowflake_id
